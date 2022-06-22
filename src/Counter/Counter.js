04b715297649c039a255cb6e4a1487e9de26bb66
unknown <ApxAdmin@DESKTOP-8EDA4TE.apex.local>
2022-06-21 19:57:54 -0500

    cleaning comments

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -1,41 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { decrement, increment, sign_in } from "../actions";
 
-// //STORE -> globalized state
-
-// //ACTION
-// const increment = () => {
-//   return {
-//     type: "increment",
-//   };
-// };
-
-// const decrement = () => {
-//   return {
-//     type: "decrement",
-//   };
-// };
-
-// //REDUCER
-// const counter = (state = 0, action) => {
-//   switch (action.type) {
-//     case "increment":
-//       return state + 1;
-//     case "decrement":
-//       return state - 1;
-//   }
-// };
-
-// //console
-
-// let store = createStore(counter)
-
-// store.subscribe(() => console.log(store.getState()))
-
-// store.dispatch(increment())
-
-//DISPATCH
-export const Counter = () => {
+ export const Counter = () => {
   const counter = useSelector((state) => state.counter);
   const logged = useSelector((state) => state.logged);
   const dispatch = useDispatch();
